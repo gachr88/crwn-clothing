6c498ceca4d4e8b95c9a5ba95725c528f6e50947
gachr88 <gachr@LAPTOP-BNB7HGKL.localdomain>
2022-01-07 15:35:09 -0600

    Separete in differents components and add styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Dictory from '../../components/directory/directory.component';
+const HomePage = () => (
+    <div className="homepage">
+        <Dictory />
+    </div>
+)
+
+export default HomePage;
